76917b41244ad66fd8bb0589acf75a818d87edb3
Lars Novikov <larsnovikov@yandex.ru>
2019-03-24 20:29:35 +0200

    postgre untested carcass

---
+++ model_examples/postgresql/user.go
@@ -0,0 +1,84 @@
+package slave
+
+import (
+	"go-binlog-replication/src/constants"
+	"go-binlog-replication/src/helpers"
+	"time"
+)
+
+type User struct {
+	Id      int       `gorm:"column:id"`
+	Name    string    `gorm:"column:name"`
+	Status  string    `gorm:"column:status"`
+	Created time.Time `gorm:"column:created"`
+}
+
+func (user *User) BeforeSave() bool {
+	user.Status = "***"
+
+	return true
+}
+
+func (user *User) ParseKey(row []interface{}) {
+	user.Id = int(row[0].(int32))
+}
+
+func (User) TableName() string {
+	return "user"
+}
+
+func (User) SchemaName() string {
+	return helpers.GetCredentials(constants.DBMaster).DBname
+}
+
+func (User) getType() string {
+	return helpers.GetCredentials(constants.DBSlave).Type
+}
+
+func (user *User) Insert() bool {
+	query := `INSERT INTO ` + user.TableName() + `(id, name, status, created) VALUES(?, ?, ?, ?);`
+	params := []interface{}{
+		user.Id,
+		user.Name,
+		user.Status,
+		user.Created,
+	}
+
+	res := helpers.Exec(user.getType(), map[string]interface{}{
+		"query":  query,
+		"params": params,
+	})
+
+	return res
+}
+
+func (user *User) Update() bool {
+	query := `UPDATE ` + user.TableName() + ` SET name=?, status=?, created=? WHERE id=?;`
+	params := []interface{}{
+		user.Name,
+		user.Status,
+		user.Created,
+		user.Id,
+	}
+
+	res := helpers.Exec(user.getType(), map[string]interface{}{
+		"query":  query,
+		"params": params,
+	})
+
+	return res
+}
+
+func (user *User) Delete() bool {
+	query := `DELETE FROM ` + user.TableName() + ` WHERE id=?`
+	params := []interface{}{
+		user.Id,
+	}
+
+	res := helpers.Exec(user.getType(), map[string]interface{}{
+		"query":  query,
+		"params": params,
+	})
+
+	return res
+}
---
+++ src/helpers/postgresql.go
@@ -0,0 +1,59 @@
+package helpers
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/jmoiron/sqlx"
+	"github.com/siddontang/go-log/log"
+	"strconv"
+)
+
+type postgresqlConnection struct {
+	base *sql.DB
+}
+
+func (conn postgresqlConnection) Ping() bool {
+	if conn.base.Ping() != nil {
+		return true
+	}
+
+	return false
+}
+
+func (conn postgresqlConnection) Exec(params map[string]interface{}) bool {
+	_, err := conn.base.Exec(fmt.Sprintf("%v", params["query"]), makeSlice(params["params"])...)
+	if err != nil {
+		// TODO Надо проверять почему произошла ошибка.
+		// Если duplicate on insert - игнорить
+		// Поменялась структура - паниковать
+		return false
+	}
+
+	return true
+}
+
+func (conn postgresqlConnection) Get(params map[string]interface{}) *sql.Rows {
+	rows, err := conn.base.Query(fmt.Sprintf("%v", params["query"]), makeSlice(params["params"])...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return rows
+}
+
+func GetPostgresqlConnection(connection Connection, dbName string) interface{} {
+	if connection == nil || connection.Ping() == true {
+		cred := GetCredentials(dbName)
+		conn, err := sql.Open("postgres", buildPostgresqlString(cred))
+		if err != nil {
+			log.Fatal(err)
+		}
+		connection = sqlConnection{conn}
+	}
+
+	return connection
+}
+
+func buildPostgresqlString(cred Credentials) string {
+	return "host=" + cred.Host + " port=" + strconv.Itoa(cred.Port) + " user=" + cred.User + " password=" + cred.Pass + " dbname=" + cred.DBname + " sslmode=disable"
+}
