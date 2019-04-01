9e872878b9b15be94b808e0139b1c3adf5b53bb2
Lars Novikov <larsnovikov@yandex.ru>
2019-04-01 06:02:03 +0200

    table to model in messages

--- src/constants/messages.go
+++ src/constants/messages.go
@@ -2,8 +2,8 @@ package constants
 
 const (
 	MessagePosFrom      = "Get pos from %s. Pos: %s; Name: %s"
-	MessageDeleted      = "[time: %v][table: %s][pos: %v] delete row \n"
-	MessageInserted     = "[time: %v][table: %s][pos: %v] insert row\n"
-	MessageUpdated      = "[time: %v][table: %s][pos: %v] update row\n"
+	MessageDeleted      = "[time: %v][model: %s][pos: %v] delete row \n"
+	MessageInserted     = "[time: %v][model: %s][pos: %v] insert row\n"
+	MessageUpdated      = "[time: %v][model: %s][pos: %v] update row\n"
 	MessageRetryConnect = "Retry to connect to db \"%s\" after %s seconds..."
 )
